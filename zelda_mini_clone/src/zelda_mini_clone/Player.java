61b99f1300bdf0bf7ef1474be2a1e30e373f0ee5
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2023-07-07 01:32:02 -0300

    feat(animations): change between sprites when moving down

--- zelda_mini_clone/src/zelda_mini_clone/Player.java
+++ zelda_mini_clone/src/zelda_mini_clone/Player.java
@@ -1,6 +1,5 @@
 package zelda_mini_clone;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
@@ -8,25 +7,46 @@ public class Player extends Rectangle {
 	public int speed = 4;
 	public boolean right, up, down, left;
 	
+	public int currentAnimation = 0;
+	
+	public int currentFrames = 0, targetFrames = 15;
+	
 	public Player(int x, int y) {
 		super(x, y, 32, 32); // player size and position
 	}
 
 	public void tick() { // player movement rules
+		boolean moved = false;
+		
 		if(right && World.isFree(x+speed, y)) {
 			x+=speed;
+			moved = true;
 		} else if(left && World.isFree(x-speed, y)) {
 			x-=speed;
+			moved = true;
 		}
 		
 		if(up && World.isFree(x, y-speed)) {
 			y-=speed;
+			moved = true;
 		} else if(down && World.isFree(x, y+speed)) {
 			y+=speed;
+			moved = true;
+		}
+		
+		if(moved) {
+			currentFrames++;
+			if(currentFrames == targetFrames) {
+				currentFrames = 0;
+				currentAnimation++;
+				if(currentAnimation == Spritesheet.player_front.length) {
+					currentAnimation = 0;
+				}
+			}
 		}
 	}
 	
 	public void render(Graphics g) { // render player color in x and y position with 32px of size
-		g.drawImage(Spritesheet.player_front, x, y, 32, 32, null);
+		g.drawImage(Spritesheet.player_front[currentAnimation], x, y, 32, 32, null);
 	}
 }
--- zelda_mini_clone/src/zelda_mini_clone/Spritesheet.java
+++ zelda_mini_clone/src/zelda_mini_clone/Spritesheet.java
@@ -8,7 +8,7 @@ import javax.imageio.ImageIO;
 public class Spritesheet {
 	public static BufferedImage spritesheet;
 
-	public static BufferedImage player_front; 
+	public static BufferedImage[] player_front; 
 	public static BufferedImage tile_wall; 
 	
 	public Spritesheet() {
@@ -18,8 +18,11 @@ public class Spritesheet {
 			e.printStackTrace();
 		}
 		
-		player_front = Spritesheet.getSprite(0, 11, 16, 16); // define position of sprite in sprite sheet
-		tile_wall = Spritesheet.getSprite(280, 221, 16, 16);
+		player_front = new BufferedImage[2];
+		player_front[0] = Spritesheet.getSprite(0, 11, 16, 16);
+		player_front[1] = Spritesheet.getSprite(16, 11, 16, 16);
+		
+		tile_wall = Spritesheet.getSprite(280, 221, 16, 16);  // define position of sprite in sprite sheet
 	}
 	
 	public static BufferedImage getSprite(int x, int y, int width, int heigth) {
